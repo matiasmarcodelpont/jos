db7e1375e2b23e117f0276d29441e1114ce28523
ngaido <nicolasgaido1@gmail.com>
2020-08-30 13:43:11 -0300

    Start Parte 1: cache de bloques

--- fs/bc.c
+++ fs/bc.c
@@ -51,6 +51,9 @@ bc_pgfault(struct UTrapframe *utf)
 	//
 	// LAB 5: you code here:
 
+	void* rounded_addr = ROUNDDOWN(addr,PGSIZE);
+	ide_read(blockno * BLKSECTS, rounded_addr, BLKSECTS);
+
 	// Clear the dirty bit for the disk block page since we just read the
 	// block from disk
 	if ((r = sys_page_map(0, addr, 0, addr, uvpt[PGNUM(addr)] & PTE_SYSCALL)) <
@@ -80,7 +83,21 @@ flush_block(void *addr)
 		panic("flush_block of bad va %08x", addr);
 
 	// LAB 5: Your code here.
-	panic("flush_block not implemented");
+
+	void *rounded_addr = ROUNDDOWN(addr,PGSIZE);
+
+	if(!va_is_mapped(rounded_addr))
+		return;
+	if(!va_is_dirty(rounded_addr))
+		return;
+
+	ide_write(blockno * BLKSECTS,rounded_addr,BLKSECTS);
+
+	int r;
+	if ((r = sys_page_map(0, addr, 0, addr, uvpt[PGNUM(addr)] & PTE_SYSCALL)) <
+	    0)
+		panic("in bc_pgfault, sys_page_map: %e", r);
+
 }
 
 // Test that the block cache works, by smashing the superblock and
