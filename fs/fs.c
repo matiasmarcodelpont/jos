2f828f060c8fc26bb8a31cbdea6243d0aa547796
Matías Marcó del Pont <mmarco@fi.uba.ar>
2020-08-31 00:01:13 -0300

    Tarea: alloc_block

--- fs/fs.c
+++ fs/fs.c
@@ -61,8 +61,12 @@ alloc_block(void)
 	// contains the in-use bits for BLKBITSIZE blocks.  There are
 	// super->s_nblocks blocks in the disk altogether.
 
-	// LAB 5: Your code here.
-	panic("alloc_block not implemented");
+	for (int i = 0; i < super->s_nblocks; i++)
+		if (bitmap[i / 32] & (1 << (i % 32))) {
+			bitmap[i / 32] &= ~(1 << (i % 32));
+			return i;
+		}
+
 	return -E_NO_DISK;
 }
 
